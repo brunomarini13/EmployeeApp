516036f570d94ccacdbe90cb1e4a43489ea6e8d1
Bruno <brunook13@outlook.com>
2021-09-19 16:04:11 -0300

    Add project files.

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        private string empSSN;
+
+        // Properties
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                {
+                    Console.WriteLine("Error! Name lenght exceeds 15 characters!");
+                }
+                else
+                {
+                    empName = value;
+                }
+            }
+        }
+
+        public int ID
+        {
+            get { return empID; }
+            set
+            {
+                if (value > 0)
+                    empID = value;
+            }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set
+            {
+                if (value > 1000f)
+                    currPay = value;
+                else
+                    currPay = 1000f;
+            }
+        }
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+        public string SSN
+        {
+            get { return empSSN; }
+        }
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay, string ssn)
+        : this(name, 0, id, pay, ssn) { }
+        public Employee(string name, int age, int id, float pay, string ssn)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+            Pay = pay;
+            empSSN = ssn;
+        }
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
+            Console.WriteLine("Social security number: {0}", SSN);
+        }
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Bruno", 22, 456, 100, "8484848");
+            emp.GiveBonus(1000);
+            emp.Age++;
+            emp.DisplayStats();
+            emp.Name = "Bruninho";
+            string test = emp.Name;
+            Console.WriteLine("Employee is named: {0}", emp.Name);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
